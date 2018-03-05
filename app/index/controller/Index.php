f3494f3d3a80b54dc05fb988919bb1e22ea448e6
aboutimes <aboutime2012@gmail.com>
2018-03-05 17:29:16 +0800

    config merge test

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -5,6 +5,12 @@ class Index
 {
     public function index()
     {
-        return 'hello world';
+        // return 'hello world';
+        // dump(config());
+        $config_a = ['a' => 'wee'];
+        $config_b = ['a' => 'are'];
+        $config_c = ['c' => 'are'];
+        dump(array_merge($config_a, $config_b));
+        dump(array_merge($config_a, $config_c));
     }
 }
---
+++ conf/config.php
@@ -0,0 +1,5 @@
+<?php
+  return [
+    'app_author' => 'Couh',
+    'app_debug' => 'true',
+  ];
