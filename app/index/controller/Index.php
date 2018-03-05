0e08601baeef733d40a725c6b1e93a42f9c7d7cb
aboutimes <aboutime2012@gmail.com>
2018-03-05 21:08:29 +0800

    api test

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -1,16 +1,20 @@
 <?php
 namespace app\index\controller;
+use think\Config;
+use think\Env;
 
 class Index
 {
     public function index()
     {
-        // return 'hello world';
-        // dump(config());
-        $config_a = ['a' => 'wee'];
-        $config_b = ['a' => 'are'];
-        $config_c = ['c' => 'are'];
-        dump(array_merge($config_a, $config_b));
-        dump(array_merge($config_a, $config_c));
+        return 'hello world';
+        // dump($_ENV);
+        // $config_a = ['a' => 'wee'];
+        // $config_b = ['a' => 'are'];
+        // $config_c = ['c' => 'are'];
+        // dump(array_merge($config_a, $config_b));
+        // dump(array_merge($config_a, $config_c));
+        // dump(Env::get('email'));
+        
     }
 }
--- conf/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-  return [
-    'app_author' => 'Couh',
-    'app_debug' => 'true',
-  ];
