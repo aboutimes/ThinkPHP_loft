19cde25c9ad35781f3f16a6b6c500576cab4c369
aboutimes <aboutime2012@gmail.com>
2018-03-21 21:20:15 +0800

    parameter passing

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -2,12 +2,17 @@
 namespace app\index\controller;
 use think\Config;
 use think\Env;
+use think\Controller;
+use think\View;
 
 class Index
 {
     public function index()
     {
-        return 'hello world';
+        $test = 'ew';
+        $test2 = ['name' => 'helloworld'];
+//        $b = DB::table()
+        return view('index',['test'=>$test,'test2' => $test2]);
         // dump($_ENV);
         // $config_a = ['a' => 'wee'];
         // $config_b = ['a' => 'are'];
@@ -15,6 +20,7 @@ class Index
         // dump(array_merge($config_a, $config_b));
         // dump(array_merge($config_a, $config_c));
         // dump(Env::get('email'));
-        
+
     }
+
 }
