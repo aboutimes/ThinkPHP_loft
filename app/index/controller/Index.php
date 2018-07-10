38360e2a98a11d5dc77e6cb083ad9616960d8950
aboutimes <aboutime2012@gmail.com>
2018-07-10 10:25:34 +0800

    update license

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -4,6 +4,7 @@ use think\Config;
 use think\Env;
 use think\Controller;
 use think\View;
+use think\Db;
 
 class Index
 {
@@ -11,8 +12,10 @@ class Index
     {
         $test = 'ew';
         $test2 = ['name' => 'helloworld'];
-//        $b = DB::table()
-        return view('index',['test'=>$test,'test2' => $test2]);
+        $b = Db::table('blog_article')->where('arc_id', 1)->find();
+        print_r($b);
+
+        return view('index',['test'=>$test,'test2' => $test2,'b' => $b]);
         // dump($_ENV);
         // $config_a = ['a' => 'wee'];
         // $config_b = ['a' => 'are'];
